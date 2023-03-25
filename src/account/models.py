ff373f2a0dc936d343cc0963ee690a312deca5b0
dori-dev <mr.dori.dev@gmail.com>
2023-03-25 22:51:10 +0330

    add user model

--- src/account/models.py
+++ src/account/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
+    phone = models.CharField(
+        max_length=11,
+        unique=True,
+    )
+    full_name = models.CharField(
+        max_length=64,
+    )
+    is_active = models.BooleanField(
+        default=True,
+    )
+    is_admin = models.BooleanField(
+        default=False,
+    )
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = [
+        'email',
+        'full_name',
+    ]
+
+    def __str__(self) -> str:
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
