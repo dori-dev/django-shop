191f1356df72be0ff577b0388224ef0b2ff6e380
dori-dev <mr.dori.dev@gmail.com>
2023-03-27 20:35:16 +0330

    add user creation form

---
+++ src/account/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from account import models
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label='Confirm Password',
+        widget=forms.PasswordInput,
+    )
+
+    class Meta:
+        model = models.User
+        fields = [
+            'email',
+            'phone',
+            'full_name',
+        ]
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Password2 don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password1'))
+        if commit:
+            user.save()
+        return user
