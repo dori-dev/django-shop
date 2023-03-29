a54b5edfe76d71201d69cd0cd00f7f15f1c46699
dori-dev <mr.dori.dev@gmail.com>
2023-03-29 19:47:47 +0330

    send otp with kavenegar

--- src/account/utils.py
+++ src/account/utils.py
@@ -1,2 +1,17 @@
+from django.conf import settings
+from kavenegar import KavenegarAPI, APIException, HTTPException
+
+
 def send_otp_code(phone, code):
-    pass
+    try:
+        api = KavenegarAPI(settings.KAVENEGAR_API_KEY)
+        params = {
+            'sender': '',
+            'receptor': phone,
+            'message': f'کد تایید شما: {code}\nفروشگاه دری شاپ',
+        }
+        api.sms_send(params)
+    except APIException as err:
+        print(err)
+    except HTTPException as err:
+        print(err)
