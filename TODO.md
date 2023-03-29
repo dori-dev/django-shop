fe288d48d797c1a96db76553508129e7d72a069f
dori-dev <mr.dori.dev@gmail.com>
2023-03-29 22:56:09 +0330

    add media files

--- TODO.md
+++ TODO.md
@@ -1,2 +1,10 @@
 expire in otp
 5 times enter code
+login
+user creation form
+    def link(self, model):
+        url = model.get_absolute_url()
+        return format_html(
+            f'<a target="_blank" href="{url}">Url</a>'
+        )
+admin category and product url
--- src/config/settings.py
+++ src/config/settings.py
@@ -120,17 +120,17 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+# Static files
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
+# Default primary key f`ield type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.User'
