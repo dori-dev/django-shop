6463aaf79372cc5ddaec70d730312e389cc7f7c5
dori-dev <mr.dori.dev@gmail.com>
2023-04-02 14:32:46 +0330

    complete cart add view

--- src/order/views/add.py
+++ src/order/views/add.py
@@ -2,12 +2,20 @@ from django.shortcuts import redirect, get_object_or_404
 from django.views import View
 
 from product.models import Product
+from order.cart import Cart
+from order.forms import CartAddForm
 
 
 class CartAddView(View):
     def post(self, request, slug):
+        form = CartAddForm(request.POST)
         product = get_object_or_404(
             Product,
             slug=slug,
         )
-        return redirect(product.get_absolute_url())
+        quantity = 1
+        if form.is_valid():
+            quantity = form.cleaned_data.get('quantity')
+        cart = Cart(request)
+        cart.add(product, quantity)
+        return redirect('order:cart')
